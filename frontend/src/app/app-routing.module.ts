84019bc21e66f26727f7c803b90be464736a5d66
Archishmaan Upadhyaya <archishmaan.upa4@gmail.com>
2024-01-17 12:51:28 +0530

    created routes and added router-outlet

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { ErrorpageComponent } from './errorpage/errorpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: LoginComponent
+  },
+  {
+    path: "signup", component: SignupComponent
+  },
+  {
+    path: "login", component: LoginComponent
+  },
+  {
+    path: "**", component: ErrorpageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
